7ab9b7a4bda931e5e1b79dec38b58d376447d097
Jess <jessicapeter92@gmail.com>
2020-06-23 21:23:26 +0100

    styled comparator chart

--- comparativeChart.js
+++ comparativeChart.js
@@ -2,14 +2,26 @@ function drawComparativeChart(holder, data, clickData, defaultName, title){
 	let div = d3.select(holder);
 	div.select("h3").html(title);
 
+	const t = div.transition()
+        .duration(1000);
+
 	let list = div.select("ul")
 		.selectAll("li")
-		.data(data[defaultName])
-		.join("li")
-		.html(d => d)
-		.on("click", function(d){
-			d3.select(clickData + " select").node().slim.set(d);
-		});
+		.data(data[defaultName], d => d)
+			.join(
+	        enter => enter.append("li")
+	            .style("opacity", 0).style("margin-left", "-25px")
+	            .html(d => "<span>" + d + "</span>")
+	            .on("click", function(d){
+					d3.select(clickData + " select").node().slim.set(d);
+				})
+	          .call(enter => enter.transition(t)
+	            .style("opacity", 1).style("margin-left", "0px")),
+	        update => update,
+	        exit => exit
+	          .call(exit => exit
+	            .remove())
+	      );
 }
 
 function drawComparativeSelector(holder, data, clickData, defaultName, title){
